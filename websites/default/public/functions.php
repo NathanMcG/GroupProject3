0bfbd580d5c55166a565ba83fdb28768d97663e5
NathanMcG <nathan.mcgaughey99@gmail.com>
2020-01-15 12:20:46 +0000

    Functions

--- websites/default/public/functions.php
+++ websites/default/public/functions.php
@@ -3,13 +3,21 @@
     //Database Commands
 
     function pdo(){
-        $pdo = 
+        $server = 'v.je';
+        $username = 'student';
+        $password = 'student';
+        //The name of the schema we created earlier in MySQL workbench
+        //If this schema does not exist you will get an error!$
+        $schema = 'LastOrders';
+
+        $pdo = new PDO('mysql:dbname=' . $schema . ';host=' . $server, $username, $password,[ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        
         return $pdo;
     }
 
     //Formatting
 
-    
+
 
     //searches
 
