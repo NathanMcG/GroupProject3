099ce8676db0c11a6c08b7d20e8490e333297986
NathanMcG <nathan.mcgaughey99@gmail.com>
2020-01-22 11:59:50 +0000

    Update functions.php

--- default/public/functions.php
+++ default/public/functions.php
@@ -6,8 +6,6 @@
         $server = 'v.je';
         $username = 'student';
         $password = 'student';
-        //The name of the schema we created earlier in MySQL workbench
-        //If this schema does not exist you will get an error!$
         $schema = 'LastOrders';
 
         $pdo = new PDO('mysql:dbname=' . $schema . ';host=' . $server, $username, $password,[ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
@@ -21,6 +19,6 @@
 
     //searches
 
-    
+
 
 ?>
