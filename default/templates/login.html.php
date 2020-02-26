2d60235704fca4e1ffa863c8f20184fa1cd7482f
deanF <farah.dean18@my.northampton.ac.uk>
2020-02-26 04:47:36 +0000

    Farah - login form (draft)

---
+++ default/templates/login.html.php
@@ -0,0 +1,18 @@
+<div class="login-form">
+  <form>
+    <ul class="login-flex">
+      <li>
+        <label for="username" class="login-label">Username</label>
+        <input type="text" id="username" class="login-textbox" placeholder="Enter your username"/>
+      </li>
+      <li>
+        <label for="password" class="login-label">Password</label>
+        <input type="text" id="password" class="login-textbox" placeholder="Enter your password"/>
+      </li>
+ 
+      <li>
+        <input type="submit" name="login" value="Login" />
+      </li>
+    </ul>
+  </form>
+</div>
