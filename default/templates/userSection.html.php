83e77e1a4999e0d4a32eebe7b6a85868dac1bcc6
deanF <farah.dean18@my.northampton.ac.uk>
2020-03-11 01:45:02 +0000

    Farah User Section Page

---
+++ default/templates/userSection.html.php
@@ -0,0 +1,11 @@
+<div class="user-home">
+    <div class="user-row">
+        <div class="user-col"><section class="user-box"><i class="fas fa-user"></i> <a href="#" class="user-options">Update Details</a> </section></div>
+        <div class="user-col"><section class="user-box"><i class="fas fa-plus"></i> <a href="#" class="user-options">Add Product</a> </section></div>
+    </div>
+    <div class="user-row">
+        <div class="user-col"><section class="user-box"><i class="fas fa-box-open"></i> <a href="#" class="user-options">Orders</a> </section> </div>
+       <div class="user-col"><section class="user-box"><i class="fas fa-wine-bottle"></i> <a href="#" class="user-options">User Products</a>  </section> </div>
+        
+    </div> 
+</div>
