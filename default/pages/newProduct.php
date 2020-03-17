e9351fd6d66779a2c8050b9b2cc1981655cda9de
NathanMcG <45010280+NathanMcG@users.noreply.github.com>
2020-03-17 18:14:35 +0000

    Nathan

--- default/pages/newProduct.php
+++ default/pages/newProduct.php
@@ -5,6 +5,28 @@
     $content = loadTemplate('../templates/addProducts.html.php',$variables);
 
     if(isset($_POST['product'])){
-        uploadImage('adverts','01');
+
+        $_POST['product']['volume'] = $_POST['volume'] . $_POST['unit'];
+
+        if(isset($_GET['PID'])){
+            $productsTable = new DatabaseTable('products',$_GET['PID']);
+        }
+        else{
+            $productsTable = new DatabaseTable('products',null)
+        }
+
+        $productsTable->save($_POST['products']);
+
+        if(isset($_GET['PID'])){
+            uploadImage('products',$_GET['PID']);
+        }
+        else{
+            $products = $productsTable->findAll();
+            uploadImage('products',$products[0]['product_id']);
+            $productsTable = new DatabaseTable('products',null)
+        }
+        
+
+        var_dump($_POST);
     }
     
--- default/templates/addProducts.html.php
+++ default/templates/addProducts.html.php
@@ -2,39 +2,52 @@
    <form action="?page=newProduct" method="POST" enctype="multipart/form-data">
      <div class="inner-form">
        <label for="product-name">Product Name </label>
-       <input type="text" name="product['product-name']" id="product-name" />
+       <input type="text" name="product['product_name']" id="product-name" required />
      </div>
      <div class="inner-form">
        <label for="product-descrip">Product Description </label>
-       <textarea id="product-descrip" rows="4" cols="17" name="product['product_description']"></textarea>
+       <textarea id="product-descrip" rows="4" cols="17" name="product['product_description']" required ></textarea>
+     </div>
+     <div class="inner-form">
+        <label for="product-vol">Type </label>
+        <select id="product_tpe" name="product['type_name']">
+        <?php
+          $typesTable = new DatabaseTable('types',null);
+          $types = $typesTable->findAll();
+          $classTable = new DatabaseTable('classifications',null);
+          foreach($types as $type){
+            echo '<option value="' . $type['type_name'] . '">' . $type['classification_name'] . ': ' . $type['type_name'] . '</option>';
+          }
+        ?>
+        </select> 
      </div>
      <div class="inner-form">
       <label for="product-dim">Product Dimensions </label>
-      <input type="text" name="product['product-dim']" id="product-dim" />
+      <input type="text" name="product['dimensions']" id="product-dim" />
      </div>
      <div class="inner-form">
         <label for="product-vol">Volume </label>
-        <input type="text" name="product['product-vol']" id="product-vol" />
-        <select id="volume" name="product['volume']">
-            <option value="">L</option>
-            <option value="">ML</option>
+        <input type="text" name="volume" id="product-vol" />
+        <select id="volume" name="unit">
+            <option value="L">L</option>
+            <option value="ml">ml</option>
         </select> 
      </div>
      <div class="inner-form">
        <label for="product-alc">Alcohol Content </label>
-       <input type="text" name="product['product-alc']" id="product-alc" />
+       <input type="text" name="product['alcohol_content']" id="product-alc" />
      </div>
      <div class="inner-form">
        <label for="product-brand">Brand </label>
-       <input type="text" required name="product['product-brand']" id="product-brand" />
+       <input type="text" name="product['brand']" id="product-brand" />
      </div>
      <div class="inner-form">
       <label for="product-offer">Offer </label>
-      <input type="text" name="product['product-offer']" id="product-offer" />
+      <input type="text" name="product['product_discount']" id="product-offer" />
      </div>
      <div class="inner-form">
       <label for="product-price">Price </label>
-      <input type="text" name="product['product-price']" id="product-price" />
+      <input type="text" name="product['product_price']" id="product-price" required />
      </div>
      <div class="inner-form">
       <label for="productImage">Image </label>
