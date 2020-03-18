787ed5481ad488fe4c2d19c434eb3339876ba26a
deanF <farah.dean18@my.northampton.ac.uk>
2020-03-18 03:25:01 +0000

    Farah

---
+++ default/templates/searchList.html.php
@@ -0,0 +1,26 @@
+<div class="search-container">
+    <ul class="search-results">
+        <li>
+            <img src="images/bottle.png" alt="product image" class="img-product" width="100px" height="100px">
+            <article>
+                <a href="">Alcohol Name</a>
+                <label>£10.00</label>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sodales ornare purus, non laoreet dolor sagittis id. 
+                Vestibulum lobortis laoreet nibh, eu luctus purus volutpat sit amet. Proin nec iaculis nulla. Vivamus nec
+                tempus quam, sed dapibus massa. 
+                </p>
+            </article>
+        </li>
+        <li>
+            <img src="images/bottle.png" alt="product image" class="img-product" width="100px" height="100px">
+            <article>
+                <a href="">Alcohol Name</a>
+                <label>£10.00</label>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sodales ornare purus, non laoreet dolor sagittis id. 
+                Vestibulum lobortis laoreet nibh, eu luctus purus volutpat sit amet. Proin nec iaculis nulla. Vivamus nec
+                tempus quam, sed dapibus massa. 
+                </p>
+            </article>
+        </li>
+    </ul>
+</div>
---
+++ default/templates/userOrders.html.php
@@ -0,0 +1,64 @@
+<div class="user-order-details">
+    <ul class="orders-list">
+        <li>
+            <img src="images/bottle.png" alt="product image" class="img-product" width="100px" height="100px">
+            <article>
+                <h2>Alcohol Name</h2>
+                <label><strong>Price:</strong> £10.00</label>
+                <label><strong>Quantity:</strong> 1</label>
+                <label><strong>Order Date:</strong> 21/02/2020</label>
+                <label><strong>Delivery Date:</strong> 21/03/2020</label>
+            </article>
+        </li>
+        <li>
+            <img src="images/bottle.png" alt="product image" class="img-product" width="100px" height="100px">
+            <article>
+            <h2>Alcohol Name</h2>
+                <label><strong>Price:</strong> £10.00</label>
+                <label><strong>Quantity:</strong> 1</label>
+                <label><strong>Order Date:</strong> 21/02/2020</label>
+                <label><strong>Delivery Date:</strong> 21/03/2020</label>
+            </article>
+        </li>
+        <li>
+            <img src="images/bottle.png" alt="product image" class="img-product" width="100px" height="100px">
+            <article>
+                <h2>Alcohol Name</h2>
+                <label><strong>Price:</strong> £10.00</label>
+                <label><strong>Quantity:</strong> 1</label>
+                <label><strong>Order Date:</strong> 21/02/2020</label>
+                <label><strong>Delivery Date:</strong> 21/03/2020</label>
+            </article>
+        </li>
+        <li>
+            <img src="images/bottle.png" alt="product image" class="img-product" width="100px" height="100px">
+            <article>
+            <h2>Alcohol Name</h2>
+                <label><strong>Price:</strong> £10.00</label>
+                <label><strong>Quantity:</strong> 1</label>
+                <label><strong>Order Date:</strong> 21/02/2020</label>
+                <label><strong>Delivery Date:</strong> 21/03/2020</label>
+            </article>
+        </li>
+        <li>
+            <img src="images/bottle.png" alt="product image" class="img-product" width="100px" height="100px">
+            <article>
+                <h2>Alcohol Name</h2>
+                <label><strong>Price:</strong> £10.00</label>
+                <label><strong>Quantity:</strong> 1</label>
+                <label><strong>Order Date:</strong> 21/02/2020</label>
+                <label><strong>Delivery Date:</strong> 21/03/2020</label>
+            </article>
+        </li>
+        <li>
+            <img src="images/bottle.png" alt="product image" class="img-product" width="100px" height="100px">
+            <article>
+            <h2>Alcohol Name</h2>
+                <label><strong>Price:</strong> £10.00</label>
+                <label><strong>Quantity:</strong> 1</label>
+                <label><strong>Order Date:</strong> 21/02/2020</label>
+                <label><strong>Delivery Date:</strong> 21/03/2020</label>
+            </article>
+        </li>
+    </ul>
+</div>
