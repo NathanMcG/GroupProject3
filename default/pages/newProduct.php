cec1549865053ee2555e1984dadc163c3065c66e
NathanMcG <45010280+NathanMcG@users.noreply.github.com>
2020-04-02 22:53:20 +0100

    Nathan

--- default/pages/newProduct.php
+++ default/pages/newProduct.php
@@ -17,14 +17,19 @@
             $productsTable = new DatabaseTable('products',null);
         }
 
+        echo 1;
+        var_dump($_POST['product']);
         $productsTable->save($_POST['product']);
 
-        if(isset($_GET['PID'])){
-            uploadImage('products',$_GET['PID']);
-        }
-        else{
-            $products = $productsTable->findAll();
-            uploadImage('products',$products[count($products)-1]['product_id']);
+        echo 2;
+        if(isset($_FILES['productImage'])){
+            if(isset($_GET['PID'])){
+                uploadImage('products',$_GET['PID']);
+            }
+            else{
+                $products = $productsTable->findAll();
+                uploadImage('products',$products[count($products)-1]['product_id']);
+            }
         }
         
 
