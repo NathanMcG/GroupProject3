ef0cfd2c9c28820178e7283101e6df1eb4a13b20
Denisa987 <45376124+Denisa987@users.noreply.github.com>
2020-04-09 06:59:10 +0100

    Result Page Update
    The drop-down bars and the checkbox areas have now been fixed.  Extra comments have also been added.

--- results.js
+++ results.js
@@ -1,8 +1,8 @@
 // Basket
 $(function() {
-    $('.nav-toggle').click(function(e) {
-        e.preventDefault();
-      $('#pull-out').toggleClass("hidden");
+    $('.nav-toggle').click(function(e) { //When the basket is clicked, the function below is run.
+        e.preventDefault(); // This ensures that when the user clicks the basket, the page doesn't automatically scroll to the top everytime. It is implemented to improve the user experience.
+      $('#pull-out').toggleClass("hidden"); //Runs the "hidden" class, and displays the contents of the basket for the user to view. 
     });
 });
 
