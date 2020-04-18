2657828e9e21e148085c5861c83ec511c88c814c
NathanMcG <45010280+NathanMcG@users.noreply.github.com>
2020-04-18 16:46:06 +0100

    Nathan

--- default/templates/itemAdvert.html.php
+++ default/templates/itemAdvert.html.php
@@ -3,13 +3,13 @@
     <img src="images/adverts/<?=$file_name?>" alt="product image" class="img-product" style="max-height: 100px;">
     <h2 style="max-width: 30%;margin-left: 30px;margin-top: 15px;color: #ffff;margin-right: 30px;"><?=$file_name?></h2>
 
-    <article style="padding-top: 10px;color: #ffff;">
+    <article style="padding-top: 10px;color: #ffff;max-width: 35%;">
         <label><strong>Start Date:</strong> <?=$start_date?></label>
         <label><strong>End Date:</strong> <?=$end_date?></label>
-        <label><strong>Link:</strong> <a href="<?=$link?>" style="color: #f2b662;"><?=$link?></label></a>
+        <label style="max-width:100%;height: 20%;"><strong>Link:</strong> <a href="<?=$link?>" style="color: #f2b662;overflow: hidden;"><?=$link?></label></a>
     </article>
 
-    <article style="width: 150px;height: 100%;margin-left: 20px; position: absolute;right: 10px;">
+    <article style="width: 15%;height: 100%;margin-left: 20px; position: absolute;right: 10px;">
         <form action="" method="POST" style="height: 100%;width: 100%;">
 
                 <input type="submit" name="submit" value="Edit" style="width: 100%;height: 40%;margin-top: 3.75%;margin-bottom: 2.5%;color: #fff;border: 1px solid #f2b662;background-color: #c93d67;"/>
